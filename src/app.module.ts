a61c4a0048dc28da1f83f0d0274840bcd9804023
SeraYUN <serayun@SeraYUNs-MacBook-Air.local>
2026-02-10 15:15:24 +0900

    #3 Init Login API

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,26 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { FeedsModule } from './feeds/feeds.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsersModule, FeedsModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'clone-twiteer-clone-twitter.i.aivencloud.com',
+      port: 19491,
+      username: 'avnadmin',
+      password: 'AVNS_sTd_MEbzF11BpubA97d',
+      database: 'twitter',
+      autoLoadEntities: true,
+      synchronize: false,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    }),
+    UsersModule,
+    FeedsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
