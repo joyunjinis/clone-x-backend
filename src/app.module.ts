a347348ef1afa0a4aba936393287eded3d50e44b
SeraYUN <serayun@SeraYUNs-MacBook-Air.local>
2026-02-10 15:17:25 +0900

    #3 Init Login API

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,7 +12,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       host: 'clone-twiteer-clone-twitter.i.aivencloud.com',
       port: 19491,
       username: 'avnadmin',
-      password: 'AVNS_sTd_MEbzF11BpubA97d',
+      password: 'process.env.DB_PASSWORD',
       database: 'twitter',
       autoLoadEntities: true,
       synchronize: false,
